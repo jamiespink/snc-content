56c382364970cb2cc3e121a41039e5d3a1d36794
Jamie Spink <jamiespink2@gmail.com>
2022-07-17 20:32:50 +0200

    added about section

---
+++ components/About.js
@@ -0,0 +1,23 @@
+import Container from "./Container";
+import styles from "../styles/About.module.scss";
+
+export default function About() {
+  return (
+    <section className={styles.outerContainer}>
+      <Container narrow>
+        <h2>About</h2>
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum.
+        </p>
+      </Container>
+    </section>
+  );
+}
