34dc736d4ddf2af7fdb4ea095f380ff774187c68
Jamie Spink <jamiespink2@gmail.com>
2022-07-18 19:42:00 +0200

    added footer

---
+++ components/Footer.js
@@ -0,0 +1,13 @@
+import Container from "./Container";
+import styles from "../styles/Footer.module.scss";
+
+export default function Footer() {
+  return (
+    <footer className={styles.outerContainer}>
+      <Container>
+        <h2>Need a Content Writer? Let's work together.</h2>
+        <a href="mailto:info@snc-content.com">info@snc-content.com</a>
+      </Container>
+    </footer>
+  );
+}
