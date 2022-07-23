b22e99d209ddc8b7225f2e1e68b89e6c4bd7955b
Jamie Spink <jamiespink2@gmail.com>
2022-07-23 13:41:40 +0200

    dynamic about section

--- pages/index.js
+++ pages/index.js
@@ -3,24 +3,25 @@ import Layout from "../components/Layout";
 import Hero from "../components/Hero";
 import About from "../components/About";
 import CategoriesList from "../components/CategoriesList";
-import { getCategories } from "../lib/api";
+import { getHomeData, getCategories } from "../lib/api";
 
 // const categories = ["web", "email", "social", "editorial"];
 
-export default function Home({ categories }) {
+export default function Home({ homeData, categories }) {
   return (
     <Layout>
       <Hero />
-      <About />
+      <About content={homeData.acf.about} />
       <CategoriesList categories={categories} />
     </Layout>
   );
 }
 
 export async function getServerSideProps(context) {
+  const homeData = await getHomeData();
   const categories = await getCategories();
 
   return {
-    props: { categories },
+    props: { homeData, categories },
   };
 }
