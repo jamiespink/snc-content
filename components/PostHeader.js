3ba69f8f885053ab151401408f81ff294398d7e6
Jamie Spink <jamiespink2@gmail.com>
2022-08-07 14:43:18 +0200

    styling to post header

--- components/PostHeader.js
+++ components/PostHeader.js
@@ -4,20 +4,24 @@ import styles from "../styles/PostHeader.module.scss";
 
 export default function PostHeader({ post }) {
   const imageProps = post.plaiceholder;
+  const imageStyles = {
+    opacity: 0.3
+  }
   
   return (
     <section className={styles.outerContainer}>
       <Container>
-        <h1>{post.title.rendered}</h1>
         {post._embedded["wp:featuredmedia"] && (
           <div className={styles.image}>
             <Image
               {...imageProps}
+              style={imageStyles}
               placeholder="blur"
               layout="fill"
               objectFit="cover"
               alt={post.title.rendered}
             />
+            <h1>{post.title.rendered}</h1>
           </div>
         )}
       </Container>
